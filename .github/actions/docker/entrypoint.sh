97a753d0c6d884969bc7ccb443d27d71337aa481
Andreas Wittig <andreas@widdix.de>
2020-02-17 12:09:48 +0100

    fixing docker build

--- .github/actions/docker/entrypoint.sh
+++ .github/actions/docker/entrypoint.sh
@@ -7,5 +7,5 @@ echo ${INPUT_PASSWORD} | docker login -u ${INPUT_USERNAME} --password-stdin
 
 GIT_HASH=$(git rev-parse --short "$GITHUB_SHA")
 
-docker build -t widdix/aws-deployment:$GIT_HASH
+docker build -t widdix/aws-deployment:$GIT_HASH .
 docker push widdix/aws-deployment:$GIT_HASH
