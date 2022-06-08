f32227e59d6ffc91a44baf7215662db6d568200c
Michael Wittig <michael@widdix.de>
2022-06-08 11:31:37 +0200

    fix

--- .github/actions/docker/entrypoint.sh
+++ .github/actions/docker/entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 set -ex
 
-echo "test"
-
 echo ${INPUT_PASSWORD} | docker login -u ${INPUT_USERNAME} --password-stdin
 
-GIT_HASH=$(git rev-parse --short "$GITHUB_SHA")
-
-docker build -t widdix/aws-deployment:$GIT_HASH .
-docker push widdix/aws-deployment:$GIT_HASH
+docker build -t widdix/aws-deployment:${GITHUB_SHA::8} .
+docker push widdix/aws-deployment:${GITHUB_SHA::8}
